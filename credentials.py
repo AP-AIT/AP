ee82fbd27a15ce0de8f3db2544decbe800d7c042
AP-AIT <149565417+AP-AIT@users.noreply.github.com>
2023-12-05 15:00:39 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,122 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import streamlit as st
-import imaplib
-import email
-from bs4 import BeautifulSoup
-import re
-import pandas as pd
-from io import BytesIO
-import PyPDF2
-import openpyxl
-from docx import Document
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+import base64
+import os
+import io
+import nltk
+from pdf2image import convert_from_path
+import pytesseract
+from PIL import Image
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+from nltk.tokenize import word_tokenize
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 
-# Streamlit app title
-st.header("Developed by MKSSS-AIT")
-st.title("Automate2Excel: Simplified Data Transfer")
+# Create a Streamlit web interface
+st.title('Gmail File Extractor')
 
-# Create input fields for the user, password, sender's email address
-user = st.text_input("Enter your email address")
-password = st.text_input("Enter your email password", type="password")
-sender_email = st.text_input("Enter the sender's email address")
+# Input fields for user to enter Gmail credentials and file type
+email = st.text_input('Enter your Gmail address')
+password = st.text_input('Enter your Gmail password', type='password')
+file_type = st.selectbox('Select file type', ['PDF', 'Image', 'Other'])
 
-# Dropdown for selecting the input format
-input_format = st.selectbox("Select Input Format", ["PDF", "Excel", "Word"])
+# Define the scopes and credentials for Gmail API
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+creds = None
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json')
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
 
-# Dropdown for selecting the output format
-output_format = st.selectbox("Select Output Format", ["Excel"])
+# Connect to the Gmail API
+service = build('gmail', 'v1', credentials=creds)
 
-# Function to extract information from PDF content
-def extract_info_from_pdf(pdf_content):
-    pdf_reader = PyPDF2.PdfFileReader(BytesIO(pdf_content))
-    text = ""
-    for page_num in range(pdf_reader.numPages):
-        text += pdf_reader.getPage(page_num).extractText()
-    # Add logic to extract information from 'text' variable
-    return {"Text": text}
+# Function to fetch emails and download attachments
+def fetch_and_process_emails():
+    results = service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
+    messages = results.get('messages', [])
 
-# Function to extract information from Excel content
-def extract_info_from_excel(excel_content):
-    df = pd.read_excel(BytesIO(excel_content))
-    # Add logic to extract information from the DataFrame 'df'
-    return {"Excel Data": df}
+    for message in messages:
+        msg = service.users().messages().get(userId='me', id=message['id']).execute()
+        payload = msg['payload']
+        headers = payload['headers']
+        for part in payload.get('parts', []):
+            if 'data' in part['body']:
+                data = part['body']['data']
+                file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
+                path = ''.join([os.getcwd(), '/temp/', part['filename']])
+                with open(path, 'wb') as f:
+                    f.write(file_data)
+                if part['mimeType'] == 'image/jpeg':
+                    text = perform_ocr_on_image(path)
+                    summarize_text(text)
+                else:
+                    text = read_file_content(path)
+                    summarize_text(text)
 
-# Function to extract information from Word content
-def extract_info_from_word(word_content):
-    doc = Document(BytesIO(word_content))
-    text = ""
-    for para in doc.paragraphs:
-        text += para.text + "\n"
-    # Add logic to extract information from 'text' variable
-    return {"Text": text}
+# Function to perform OCR on images
+def perform_ocr_on_image(image_path):
+    img = Image.open(image_path)
+    text = pytesseract.image_to_string(img)
+    return text
 
-# (Similar functions can be added for other document types like images, HTML, etc.)
+# Function to read file content
+def read_file_content(file_path):
+    if file_path.endswith('.pdf'):
+        images = convert_from_path(file_path)
+        text = ''
+        for img in images:
+            text += pytesseract.image_to_string(img)
+        return text
+    else:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+            return text
 
-if st.button("Fetch and Generate Output"):
-    try:
-        # URL for IMAP connection
-        imap_url = 'imap.gmail.com'
+# Function to summarize text
+def summarize_text(text):
+    sentences = sent_tokenize(text)
+    words = word_tokenize(text)
+    words = [word.lower() for word in words if word.isalnum()]
+    stop_words = set(stopwords.words("english"))
+    words = [word for word in words if word not in stop_words]
+    fdist = FreqDist(words)
+    print("Most common words:", fdist.most_common(5))
+    print("Summary:", ' '.join(sentences[:2]))
 
-        # Connection with GMAIL using SSL
-        my_mail = imaplib.IMAP4_SSL(imap_url)
-
-        # Log in using user and password
-        my_mail.login(user, password)
-
-        # Select the Inbox to fetch messages
-        my_mail.select('inbox')
-
-        # Define the key and value for email search
-        key = 'FROM'
-        value = sender_email
-        _, data = my_mail.search(None, key, value)
-
-        mail_id_list = data[0].split()
-
-        info_list = []
-
-        # Iterate through messages and extract information from attachments
-        for num in mail_id_list:
-            typ, data = my_mail.fetch(num, '(RFC822)')
-            msg = email.message_from_bytes(data[0][1])
-
-            for part in msg.walk():
-                content_type = part.get_content_type()
-
-                # Check content type based on user selection
-                if input_format == "PDF" and "application/pdf" in content_type:
-                    content = part.get_payload(decode=True)
-                    info = extract_info_from_pdf(content)
-                elif input_format == "Excel" and "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" in content_type:
-                    content = part.get_payload(decode=True)
-                    info = extract_info_from_excel(content)
-                elif input_format == "Word" and "application/msword" in content_type:
-                    content = part.get_payload(decode=True)
-                    info = extract_info_from_word(content)
-                # Add more conditions for other document types
-
-                info_list.append(info)
-
-        # Create a DataFrame from the info_list
-        df = pd.DataFrame(info_list)
-
-        # Display the data in the Streamlit app
-        st.write("Data extracted from documents:")
-        st.write(df)
-
-        # Download the DataFrame in the selected output format
-        output = BytesIO()
-        if output_format == "Excel":
-            with pd.ExcelWriter(output, engine='openpyxl') as writer:
-                df.to_excel(writer, sheet_name='Sheet1', index=False)
-            output.seek(0)
-            st.write("Downloading Excel file...")
-            st.download_button(
-                label="Download Excel File",
-                data=output,
-                key="download_excel",
-                on_click=None,
-                file_name="output_data.xlsx"
-            )
-        # Add conditions for other output formats
-
-    except Exception as e:
-        st.error(f"Error: {e}")
+# Trigger the file extraction when the user submits the form
+if st.button('Extract Files'):
+    fetch_and_process_emails()
+    st.write('Files extracted successfully!')
